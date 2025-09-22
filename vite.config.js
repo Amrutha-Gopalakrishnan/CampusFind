1e7d6a79c4583f4a60cce62c99a55bd1214fe3f9
Amrutha-Gopalakrishnan <amruthagopal16@gmail.com>
2025-09-22 22:08:51 +0530

    Updated Value

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  plugins: [
+  plugins: [react(),
     tailwindcss(),
   ],
+  esbuild: {
+    // Treat "use client" as harmless instead of failing
+    supported: {
+      'directive': true,}
 })
